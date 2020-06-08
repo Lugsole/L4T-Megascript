8a4bb7e606581f53d84973bee9b5983f25fbf652
Lugsole <33535519+Lugsole@users.noreply.github.com>
2020-06-07 22:40:59 -0400

    Update core.sh

--- core.sh
+++ core.sh
@@ -18,6 +18,16 @@ echo -e "\e[31mHello World\e[0m"
 echo -e "\x1B[31mHello World\e[0m"
 
 clear
+
+system_update () {
+	echo "Updater script successfully started! Running updates..."
+	sleep 1
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt autoremove -y
+	sudo flatpak update -y
+	echo "Done! Sending you back to the main menu..."
+}
 #echo "create a desktop file for the script on this line"
 echo "Welcome back to the main menu of the L4T Megascript, $USER. This isn't quite finished yet - we'll be ready eventually!"
 echo
@@ -27,52 +37,56 @@ sleep 2
 #echo "note to self: get lakka firmware files and put in /lib/firmware - see L4T gbatemp release thread about that"
 echo
 echo
-#echo "0...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
-echo "1...............Update your programs - download and install updates from the repos (check this at least once daily!)"
-echo "2...............Dolphin - Gamecube and Wii emulator, latest development version"
-#echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
-#echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
-#echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
-#echo "6...............Discord setup - not the actual program, but the web app"
-#echo "7...............Kodi - media center"
-#echo "8...............Development IDEs - write code on your Switch!"
-#echo "9...............Video Settings - Is your display going past the edges of your TV?"
-#echo "10..............Citra - 3DS emulator, this probably is completely broken"
-echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
-echo
-echo
-read -p "Make a selection: " userInput
+while true
+do
+	#echo "0...............Initial setup - swapfile, joycon mouse, Bluetooth audio, USB controller support, 2.0 GHz overclock, etc"
+	echo "1...............Update your programs - download and install updates from the repos (check this at least once daily!)"
+	echo "2...............Dolphin - Gamecube and Wii emulator, latest development version"
+	#echo "3...............RetroArch - Not quite working yet, I don't have a collection of working cores"
+	#echo "4...............Minecraft - automatically install Luke Chambers's MCSwitchTools (https://gitlab.com/devluke/mcswitchtools)"
+	#echo "5...............moonlight-qt - stream games from your PC as long as it has an Nvidia GPU!"
+	#echo "6...............Discord setup - not the actual program, but the web app"
+	#echo "7...............Kodi - media center"
+	#echo "8...............Development IDEs - write code on your Switch!"
+	#echo "9...............Video Settings - Is your display going past the edges of your TV?"
+	#echo "10..............Citra - 3DS emulator, this probably is completely broken"
+	echo "any other key...Close L4T Megascript, view credits and source code, and get Discord support server link"
+	echo
+	echo
+	read -p "Make a selection: " userInput
 
-echo "you have chosen $userInput"
-if [[ $userInput == 0 ]]; then
-  echo "This is where the initial setup script will be!"
-  #  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/init.sh)"
+	echo "you have chosen $userInput"
+	if [[ $userInput == 0 ]]; then
+	  echo "This is where the initial setup script will be!"
+	  #  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/init.sh)"
   
-elif [[ $userInput == 1 ]]; then
-  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/apt.sh)"
+	elif [[ $userInput == 1 ]]; then
+	  system_update
+	  
+	elif [[ $userInput == 2 ]]; then
+	  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/main.sh)"
+
+	elif [[ $userInput == 11 ]]; then
+	  	echo "Thank you for using the L4T Megascript!"
+		sleep 2
+		clear
+		echo "Credits:"
+		echo "CTRL + CLICK ON A LINK TO OPEN IT"
+		echo
+		echo -e "\e[34mCobalt - Manager/Lead Coder\e[0m"
+		
+		#echo "put other people that helped me write this here later if anyone gives some suggestions
+		echo "the Switchroot L4T Ubuntu team (https://switchroot.org/) - making the actual OS you're running right now"
+		echo "https://twitter.com/jsjfree34/ - MASSIVE help in testing and giving me the idea for this in the first 	place"
+		#echo "credit everyone else here later when it's not 2 AM"
+		echo ""
+		echo -e 'Thank you for using the L4T Megascript! Stop by our \e[36mDiscord\e[0m server at https://tinyurl.com/L4TScript for support.'
+		echo 'If that link is down for whatever reason, use https://discord.gg/UYsUFCY.'
+		echo 'Source code is available here: https://github.com/cobalt2727/L4T-Megascript/'
+		exit 0
+	else
+	  echo "Please try another option"
+	fi
 
 
-elif [[ $userInput == 2 ]]; then
-  bash -c "$(curl -s https://raw.githubusercontent.com/cobalt2727/L4T-Megascript/master/scripts/dolphin/main.sh)"
-
-
-else
-  echo ""
-fi
-
-echo "Thank you for using the L4T Megascript!"
-sleep 2
-clear
-echo "Credits:"
-echo "CTRL + CLICK ON A LINK TO OPEN IT"
-echo
-echo -e "\e[34mCobalt - Manager/Lead Coder\e[0m"
-
-#echo "put other people that helped me write this here later if anyone gives some suggestions
-echo "the Switchroot L4T Ubuntu team (https://switchroot.org/) - making the actual OS you're running right now"
-echo "https://twitter.com/jsjfree34/ - MASSIVE help in testing and giving me the idea for this in the first place"
-#echo "credit everyone else here later when it's not 2 AM"
-echo ""
-echo -e 'Thank you for using the L4T Megascript! Stop by our \e[36mDiscord\e[0m server at https://tinyurl.com/L4TScript for support.'
-echo 'If that link is down for whatever reason, use https://discord.gg/UYsUFCY.'
-echo 'Source code is available here: https://github.com/cobalt2727/L4T-Megascript/'
+done
